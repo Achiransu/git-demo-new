6aba3875fcc35d8e22aef8fe4966db2a0b79d5d6
Achiransu <57213139+Achiransu@users.noreply.github.com>
2022-01-28 12:48:28 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+#add implementation
+def add(x,y):
+    pass
+#subtract implementation
+def subtract(x,y):
+    pass
+#multiplication implementation
+def multiply(x,y):
+    pass
+#divide implementation
+def divide(x,y):
+    pass
+
+#all implemented
